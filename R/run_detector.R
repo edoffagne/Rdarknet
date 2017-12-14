add4536eb9374671a09e9806cf18c698fd9515a8
Erik Doffagne <erik.doffagne@gmail.com>
2017-12-14 21:12:56 +0100

    Passed weights as arguments from R

--- R/run_detector.R
+++ R/run_detector.R
@@ -1,6 +1,6 @@
 
-run_detector = function(z=1)
-{ x = .Call("r_run_detector", c(z))
+run_detector = function(thresh=0.3, weight_file)
+{ x = .Call("r_run_detector", thresh, weight_file)
   return(x)
 }
 
--- src/src/r_test_detector.c
+++ src/src/r_test_detector.c
@@ -5,7 +5,12 @@
 #include "darknet.h"
 #include "detector.h"
 
-SEXP r_run_detector(SEXP thresh_in) {
+SEXP r_run_detector(SEXP thresh_in, SEXP weight_file_in) {
+  PROTECT(weight_file_in = AS_CHARACTER(weight_file_in));
+  char * weight_file;
+  weight_file = R_alloc(strlen(CHAR(STRING_ELT(weight_file_in, 0))), sizeof(char));
+  strcpy(weight_file, CHAR(STRING_ELT(weight_file_in, 0)));
+  Rprintf("weight_file: %s\n", weight_file); 
 
   PROTECT(thresh_in = AS_NUMERIC(thresh_in));
   double *xT;
@@ -14,7 +19,7 @@ SEXP r_run_detector(SEXP thresh_in) {
 
   float hier_thresh = 0.5;
   int display = 0;
-  test_detector("cfg/coco.dat", "cfg/yolo.cfg", "cfg/yolo.weights", "data/dog.jpg", 
+  test_detector("cfg/coco.dat", "cfg/yolo.cfg", weight_file, "data/dog.jpg", 
                 thresh, hier_thresh, "out.jpg", display);
   UNPROTECT(1);
   return thresh_in;
